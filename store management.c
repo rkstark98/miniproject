1926d53fe82474fb288c53ba9b484f38fdff7312
rkstark98 <rkstark404@gmail.com>
2021-07-07 01:51:25 +0530

    new commit

---
+++ store management.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<windows.h>
+
+#define ANS 15
+#define ACS 4
+COORD coord= {0,0}; // this is global variable
+void gotoxy(int x,int y)
+{
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+/*declaration of checking functions*/
+void c_code(char[]);
